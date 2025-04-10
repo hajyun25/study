b562dc8c0bb6604fa7816f521489978df3c60741
hajyun25 <yhj8081@naver.com>
2025-04-10 19:57:06 +0900

    프로젝트 생성

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 윤하정 on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
